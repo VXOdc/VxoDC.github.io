bf5d4c97e94e68c0b7611f1d97d46a1652f59cb1
Noah Ly <124010810+VXOdc@users.noreply.github.com>
2026-05-03 22:25:15 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // If your URL is https://vxodc.github.io/, use '/'
+  // If your URL is https://vxodc.github.io/project-name/, use '/project-name/'
+  base: '/', 
 })
